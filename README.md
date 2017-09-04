f4bd561389a070cf39ea2b025f674d412757bc01
Hoang Ngo <hoangnv2307@gmail.com>
2017-09-04 21:31:40 +0700

    Flask-celery example proj

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# flask_celery_proj
+# Step to run flask-celery example project
+1. download redis server or rabitmq
+2. run redis/rabitmq server
+3. Start celery server with follow command:
+    celery -A flask_proj.celery worker --loglevel=info
+4. Run flask app
+    Export(Set if window) FLASK_APP=flask_proj.py
+    python -m flask run
+
+# Now you can test your app with flask and celery
---
+++ flask_proj_see_bug.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, request
+import sqlite3 as sql
+from celery import Celery
+
+
+app = Flask(__name__, template_folder= "C:\\Users\\tthn\\workspace\\Mongodb")
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/enternew')
+def new_student():
+    return render_template('student.html')
+
+@celery.task
+def students_add_task(nm, addr, city, pin):
+    error = 0
+    try:
+        with sql.connect("D:\\Working\\Projects\\flask\\flask_celery_proj\\database.db") as con:
+            cur = con.cursor()
+        
+            cur.execute("INSERT INTO students (name,addr,city,pin) VALUES (?,?,?,?)",(nm,addr,city,pin) )
+        
+            con.commit()
+    except:
+        error = 1
+        con.rollback()
+    finally:
+        con.close()
+    if error == 1:
+        raise Exception("Some error when insert")
+    
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+    if request.method == 'POST':
+        try:
+            nm = request.form['nm']
+            addr = request.form['add']
+            city = request.form['city']
+            pin = request.form['pin']
+            students_add_task.apply_async(args=[nm, addr, city, pin])
+            msg = "Record successfully added"
+        except:
+            
+            msg = "error in insert operation"
+    
+        finally:
+            return render_template("result.html",msg = msg)
+            
+@app.route('/list')
+def get_list():
+    result = students_list_task.apply_async
+    rows = result.get()
+    return render_template("list.html",rows = rows)
+
+
+@celery.task
+def students_list_task():
+    con = sql.connect("D:\\Working\\Projects\\flask\\flask_celery_proj\\database.db")
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("select * from students")
+    rows = cur.fetchall();
+    return rows
+
+if __name__ == '__main__':
+    app.run(debug = True, threaded=True)
